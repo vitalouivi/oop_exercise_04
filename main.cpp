712e100ff582ef1ba6568d1bedebb6cc523b4ef6
vitalouivi <45311390+vitalouivi@users.noreply.github.com>
2020-01-09 12:43:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <tuple>
+#include <string>
+#include <type_traits>
+#include <vector>
 #include "point.h"
 #include "trapezoid.h"
 #include "rectangle.h"
@@ -34,10 +37,22 @@ void running(std::istream& is, std::ostream& os) {
         os << center(object) << std::endl;
     }
 }
+//additional classes
+class Names {
+public:
+    std::string first;
+    std::string last;
+};
+
+class WrongNames {
+public:
+    int first;
+    int last;
+};
 
 int main() {
     char obj_type;
-    std::cout << "Input figure type: 1 - trapezoid, 2 - rhombus, 3 - rectangle, 4 - tuple or 'q' to quit" << std::endl;
+    std::cout << "Input figure type: 1 - trapezoid, 2 - rhombus, 3 - rectangle, 4 - tuple, 5 - to check additional task, or 'q' to quit" << std::endl;
     while (std::cin >> obj_type){
         if(obj_type == '4') {
             running<std::tuple<point<double>>> (std::cin, std::cout);
@@ -47,10 +62,17 @@ int main() {
 	        running<rhombus<double>>(std::cin, std::cout);
         }else if(obj_type == '3'){
 	        running<rectangle<double>>(std::cin, std::cout);
-        }else if(obj_type == 'q'){
+        }
+        else if (obj_type == '5') {
+            //additional task проверить являются ли поля класса first и last типа string
+            std::cout << HasFL<Names>::value << std::endl;
+            std::cout << HasFL<WrongNames>::value << std::endl;
+            return 0;
+        }
+        else if(obj_type == 'q'){
 	        return 0;
         }else{
 	        std::cout << "Wrong. Try 1 - trapezoid, 2 - rhombus, 3 - rectangle, 4 - tuple or 'q' to quit" << std::endl;
         }
-    }
+    }    
 }
